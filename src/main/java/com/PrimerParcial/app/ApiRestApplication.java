0a5172809d375b5750a7e0cf8f2f5dce8b915d41
Wismanu <lmhernandezv@ufpso.edu.co>
2023-04-09 16:28:54 -0500

    Hola mundo

---
+++ src/main/java/com/PrimerParcial/app/ApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.PrimerParcial.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+	}
+
+}
