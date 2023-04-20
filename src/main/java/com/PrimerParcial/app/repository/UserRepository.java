b2e99212aeb536c5d5f6cec5090e52ca2bb08f93
Wismanu <lmhernandezv@ufpso.edu.co>
2023-04-19 19:50:23 -0500

    Commit_1 creacion de arquitectura de base de datos + Creacion del proyecto apirest

---
+++ src/main/java/com/PrimerParcial/app/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.PrimerParcial.app.repository;
+import com.PrimerParcial.app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
